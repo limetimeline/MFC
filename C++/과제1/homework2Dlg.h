a33cecc9565cc09723f29116faf8d8e596808d9b
sanghyeonKim <44115202+limetimeline@users.noreply.github.com>
2021-04-10 21:10:10 +0900

    과제1

---
+++ C++/과제1/homework2Dlg.h
@@ -0,0 +1,44 @@
+﻿
+// homework2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// Chomework2Dlg 대화 상자
+class Chomework2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	Chomework2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HOMEWORK2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+//	CEdit mctrlEdit1;
+//	CEdit mctrlEdit2;
+	afx_msg void OnBnClickedButton1();
+//	CString mvalEdit2;
+//	CString mvalEdit1;
+	CEdit mctrlEdit1;
+	CString mvalEdit1;
+	CEdit mctrlEdit2;
+	CString mvalEdit2;
+};
